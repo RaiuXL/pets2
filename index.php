a06a20d10fd8dfd7e090f898d059d16672f62dbe
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-04-18 14:42:16 -0400

    Configured index to have summary page

--- index.php
+++ index.php
@@ -20,9 +20,27 @@ $F3->route('GET /', function(){
 
 // order route
 $F3->route('GET|POST /order', function($F3){
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $pet = $_POST['pet'];
+        $color = $_POST['color'];
 
+        if(!empty($pet)&&$color != 'default') {
+            echo "Please supply a pet type";
+            $F3->set('SESSION.pet',$pet);
+            $F3->set('SESSION.color',$color);
+            $F3->reroute("summary");
+        } else{
+            echo '<p>Error</p>';
+        }
+    }
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
 
+//summary route
+$F3->route('GET|POST /summary', function($F3){
+    var_dump($F3->get('SESSION'));
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
 $F3->run();
